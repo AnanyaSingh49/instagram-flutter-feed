82ceda85e73c4c0895c2adcf51e8643d8ca4232c
Ananya Singh <ananyasingh@Ananyas-MacBook-Air.local>
2026-03-15 22:52:55 +0530

    zoom funtion improved

--- lib/widgets/pinch_zoom_overlay.dart
+++ lib/widgets/pinch_zoom_overlay.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
-class PinchZoomOverlay extends StatelessWidget {
+class PinchZoomOverlay extends StatefulWidget {
   final String imageUrl;
 
   const PinchZoomOverlay({super.key, required this.imageUrl});
 
   @override
+  State<PinchZoomOverlay> createState() => _PinchZoomOverlayState();
+}
+
+class _PinchZoomOverlayState extends State<PinchZoomOverlay> {
+  final TransformationController _controller = TransformationController();
+
+  void _resetZoom() {
+    _controller.value = Matrix4.identity();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return InteractiveViewer(
-      minScale: 1.0,
-      maxScale: 4.0,
-      child: CachedNetworkImage(
-        imageUrl: imageUrl,
-        fit: BoxFit.cover,
-        width: double.infinity,
+    return GestureDetector(
+      onScaleEnd: (details) {
+        _resetZoom(); // snap back when fingers released
+      },
+      child: InteractiveViewer(
+        transformationController: _controller,
+        minScale: 1,
+        maxScale: 4,
+        panEnabled: true,
+        child: CachedNetworkImage(
+          imageUrl: widget.imageUrl,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
       ),
     );
   }
